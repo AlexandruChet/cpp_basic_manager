8d2458a9d4b40ee58a6644d9363a0628a173b3c0
radu <chetreanalexandru63@gmail.com>
2025-12-24 22:45:26 +0100

    save to file void function

--- manager/manager.cpp
+++ manager/manager.cpp
@@ -4,6 +4,7 @@
 #include <variant>
 #include <ctime>
 #include <cstdlib>
+#include <fstream>
 
 using namespace std;
 
@@ -31,6 +32,7 @@ class User : public Entity
 {
 public:
     vector<variant<int, string>> reg_information;
+    vector<string> user_products_information;
 
     User() : Entity("", "", 0, 0) {}
 
@@ -46,17 +48,40 @@ public:
         id = rand();
 
         cout << "Your id: " << id << endl;
+
+        reg_information.push_back(name);
+        reg_information.push_back(email);
+        reg_information.push_back(id);
     }
 
-    void save_to_file() override {}
+    void save_to_file() override
+    {
+        ofstream outFile("output.txt");
+
+        if (outFile.is_open())
+        {
+            for (const auto &element : reg_information)
+            {
+                if (holds_alternative<int>(element))
+                    outFile << get<int>(element) << " ";
+                else if (holds_alternative<string>(element))
+                    outFile << get<string>(element) << " ";
+            }
+        }
+        else
+        {
+            cerr << "Error" << endl;
+        }
+    }
     void save_info_to_file() override {}
     void write_info() override {}
 };
 
 int main()
 {
-    Entity* user = new User();
+    Entity *user = new User();
     user->registration();
+    user->save_to_file();
 
     delete user;
     return 0;
